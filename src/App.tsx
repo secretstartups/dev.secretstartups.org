1568114c00712e2b8af5c76e0f5f452897bc031c
Pronothurah <onsongojosphat28@gmail.com>
2025-04-16 09:46:09 +0100

    Fix: Scroll issues

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import ContactUs from "./pages/ContactUs";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 
 const queryClient = new QueryClient();
@@ -24,6 +25,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/associates" element={<Associates />} />
